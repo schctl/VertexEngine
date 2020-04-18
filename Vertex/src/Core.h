24bc35abd51fead0f233db5e57b5baca47febfe3
Sachin Cherian <sachinctl@protonmail.com>
2020-04-18 17:16:02 +0530

    try to fix build on linux

--- Vertex/src/Core.h
+++ Vertex/src/Core.h
@@ -1,13 +1,12 @@
 #pragma once
 
 #if defined(_WIN32)
-#   if defined(VX_BUILD_SHARED)
-#       define VX_PUBLIC_API __declspec(dllexport)
-#   else
-#       define VX_PUBLIC_API __declspec(dllimport)
-#   endif // VX_BUILD_SHARED
+    #define VX_PUBLIC_API __declspec(dllexport)
+
 #elif defined(__linux__) || defined(__APPLE__)
-#   define VERTEX_PUBLIC_API
+    #define VERTEX_PUBLIC_API
+
 #else
-#   error "Unknown platform. Vertex only supports windows, mac, and linux."
+    #error "Unknown platform. Vertex only supports windows, mac, and linux."
+
 #endif // _WIN32
