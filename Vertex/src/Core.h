fa60813fd2d185dd6f66c558505bb8021ac095e1
Sachin Cherian <sachinctl@protonmail.com>
2020-04-18 17:11:05 +0530

    move vx build shared define to cmake

--- Vertex/src/Core.h
+++ Vertex/src/Core.h
@@ -1,11 +1,11 @@
 #pragma once
 
-#ifdef _WIN32
-    #ifdef VX_BUILD_SHARED
+#if defined(_WIN32)
+    #if defined(VX_BUILD_SHARED)
         #define VX_PUBLIC_API __declspec(dllexport)
     #else
         #define VX_PUBLIC_API __declspec(dllimport)
-    #endif
-#else 
+    #endif // VX_BUILD_SHARED
+#else
     #define VERTEX_PUBLIC_API
-#endif
+#endif // _WIN32
