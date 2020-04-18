197c21aa5f4ae293128489fa4e4319a2e7ed7f51
Sachin Cherian <sachinctl@protonmail.com>
2020-04-18 17:14:20 +0530

    add error directive for non desktop platforms

--- Vertex/src/Core.h
+++ Vertex/src/Core.h
@@ -1,11 +1,13 @@
 #pragma once
 
 #if defined(_WIN32)
-    #if defined(VX_BUILD_SHARED)
-        #define VX_PUBLIC_API __declspec(dllexport)
-    #else
-        #define VX_PUBLIC_API __declspec(dllimport)
-    #endif // VX_BUILD_SHARED
+#   if defined(VX_BUILD_SHARED)
+#       define VX_PUBLIC_API __declspec(dllexport)
+#   else
+#       define VX_PUBLIC_API __declspec(dllimport)
+#   endif // VX_BUILD_SHARED
+#elif defined(__linux__) || defined(__APPLE__)
+#   define VERTEX_PUBLIC_API
 #else
-    #define VERTEX_PUBLIC_API
+#   error "Unknown platform. Vertex only supports windows, mac, and linux."
 #endif // _WIN32
