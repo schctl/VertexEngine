b735d2501da17ca0ef7095dfed723369b45f274a
Dinu Blanovschi <dinu.blanovschi001@gmail.com>
2020-04-19 20:48:32 +0300

    Git merge

--- Vertex/src/Core/Event/Event.h
+++ Vertex/src/Core/Event/Event.h
@@ -7,49 +7,50 @@
 #define VX_GET_EVENT_TYPE_CHAR(x) #x
 
 namespace Vertex {
+    enum class EventTypes
+    {
+        WindowResize, WindowClose,
+        KeyPress, KeyRelease,
+        MouseClick, MouseRelease, MouseScroll, MouseMove
+    };
 
-	enum class EventTypes {
-		WindowResize, WindowClose,
-		KeyPress, KeyRelease,
-		MouseClick, MouseRelease, MouseScroll, MouseMove
-	};
+    enum class EventCategories
+    {
+        WindowEvent,
+        KeyEvent,
+        MouseEvent
+    };
 
-	enum class EventCategories {
-		WindowEvent,
-		KeyEvent,
-		MouseEvent
-	};
+    class Event
+    {
+        friend class EventHandler;
 
-	class Event {
-		friend class EventHandler;
+    public:
+        virtual EventCategories GetEventCategory() = 0;
+        virtual EventTypes GetEventType() = 0;
+        virtual const char* GetEventName() = 0;
+        virtual const char* GetDetails() = 0;
+    
+    protected:
+        bool m_Handled;
+    };
 
-	public:
-		virtual EventCategories GetEventCategory() = 0;
+    class EventHandler
+    {
+    public:
+        EventHandler(Event& event)
+            : m_Event(event)
+        {
+        }
 
-		virtual EventTypes GetEventType() = 0;
+        template<typename T>
+        void Dispatch(T&& func)
+        {
+            m_Event.m_Handled = func(m_Event);
+        }
 
-		virtual const char *GetEventName() = 0;
-
-		virtual const char *GetDetails() = 0;
-
-	protected:
-		bool m_Handled;
-	};
-
-	class EventHandler {
-	public:
-		EventHandler(Event &event)
-				: m_Event(event)
-		{
-		}
-
-		void Dispatch(bool(*func)(Event))
-		{
-			m_Event.m_Handled = func(m_Event);
-		}
-
-	private:
-		Event &m_Event;
-	};
+    private:
+        Event& m_Event;
+    };
 
 }
