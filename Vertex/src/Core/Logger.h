76b33e0b9f249b41963e3ca3429e2c497ff17c7f
Sachin Cherian <sachinctl@protonmail.com>
2020-04-19 16:50:11 +0530

    try to fix build on inux

--- Vertex/src/Core/Logger.h
+++ Vertex/src/Core/Logger.h
@@ -2,8 +2,8 @@
 
 #include "Core.h"
 
-#include "spdlog/spdlog.h"
-#include "spdlog/sinks/stdout_color_sinks.h"
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
 
 #include <memory>
 
