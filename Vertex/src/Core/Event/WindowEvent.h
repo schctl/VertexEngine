d33526f454f5cdb22b3c831d6e4992890a94b7a8
Sachin Cherian <sachinctl@protonmail.com>
2020-04-20 11:36:27 +0530

    implement window events

---
+++ Vertex/src/Core/Event/WindowEvent.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include "Event.h"
+
+namespace Vertex {
+
+    class WindowEvent : public Event
+    {
+    public:
+        inline EventCategories GetEventCategory() override { return EventCategories::WindowEvent; }
+    };
+
+    class WindowResizeEvent : public WindowEvent
+    {
+    public:
+        WindowResizeEvent(unsigned int width, unsigned int height)
+            : m_Width(width), m_Height(height)
+        {
+        }
+
+        inline unsigned int GetWidth() { return m_Width; }
+        inline unsigned int GetHeight() { return m_Height; }
+
+        inline EventTypes  GetEventType() override { return EventTypes::WindowResize; }
+        inline const char* GetEventName() override { return VX_GET_EVENT_TYPE_CHAR(EventTypes::WindowResize); }
+
+        const char* GetDetails() override
+        {
+            return fmt::format("WindowResizeEvent : Width<{0}> Height<{1}>", m_Width, m_Height).c_str();
+        }
+
+    private:
+        unsigned int m_Width;
+        unsigned int m_Height;
+    };
+
+    class WindowCloseEvent : public WindowEvent
+    {
+    public:
+        WindowCloseEvent()
+        {
+        }
+
+        inline EventTypes  GetEventType() override { return EventTypes::WindowClose; }
+        inline const char* GetEventName() override { return VX_GET_EVENT_TYPE_CHAR(EventTypes::WindowClose); }
+
+        const char* GetDetails() override
+        {
+            return "WindowCloseEvent";
+        }
+    };
+
+}
