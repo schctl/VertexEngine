84ef65e754a92e53ac8815a023d0f582de2cbf3a
Sachin Cherian <sachinctl@protonmail.com>
2020-04-21 11:49:16 +0530

    make a utility class to handle layers

---
+++ Vertex/src/Core/Layer/LayerStack.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "Core/Core.h"
+
+#include "Layer.h"
+
+namespace Vertex {
+
+    class LayerStack
+    {
+    public:
+        LayerStack();
+        ~LayerStack();
+
+        void PushLayer(Layer* layer);
+        void PopLayer(Layer* layer);
+        
+        void PushOverlay(Layer* overlay);
+        void PopOverlay(Layer* overlay);
+
+        unsigned int size() { return m_Layers.size(); }
+
+        std::vector<Layer*>::iterator begin() { return m_Layers.begin(); }
+        std::vector<Layer*>::iterator end() { return m_Layers.end(); }
+
+    private:
+        std::vector<Layer*> m_Layers;
+    };
+    
+}
