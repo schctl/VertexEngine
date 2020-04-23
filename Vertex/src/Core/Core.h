2094943ae99c9840d75e5da30fde0a9e1230a8dd
Sachin Cherian <sachinctl@protonmail.com>
2020-04-23 22:11:29 +0530

    port glfw input ids to vertex

--- Vertex/src/Core/Core.h
+++ Vertex/src/Core/Core.h
@@ -13,3 +13,152 @@
 #define VX_BIND_FUNC_1(x) std::bind(&x, this, std::placeholders::_1)
 
 #define VX_CORE_ASSERT(x, ...) { if (!(x)) { Logger::GetCoreLogger()->error(__VA_ARGS__); assert(false); } }
+
+// Key IDs
+// From glfw3.h
+
+/* The unknown key */
+#define VX_KEY_UNKNOWN            -1
+
+/* Printable keys */
+#define VX_KEY_SPACE              32
+#define VX_KEY_APOSTROPHE         39  /* ' */
+#define VX_KEY_COMMA              44  /* , */
+#define VX_KEY_MINUS              45  /* - */
+#define VX_KEY_PERIOD             46  /* . */
+#define VX_KEY_SLASH              47  /* / */
+#define VX_KEY_0                  48
+#define VX_KEY_1                  49
+#define VX_KEY_2                  50
+#define VX_KEY_3                  51
+#define VX_KEY_4                  52
+#define VX_KEY_5                  53
+#define VX_KEY_6                  54
+#define VX_KEY_7                  55
+#define VX_KEY_8                  56
+#define VX_KEY_9                  57
+#define VX_KEY_SEMICOLON          59  /* ; */
+#define VX_KEY_EQUAL              61  /* = */
+#define VX_KEY_A                  65
+#define VX_KEY_B                  66
+#define VX_KEY_C                  67
+#define VX_KEY_D                  68
+#define VX_KEY_E                  69
+#define VX_KEY_F                  70
+#define VX_KEY_G                  71
+#define VX_KEY_H                  72
+#define VX_KEY_I                  73
+#define VX_KEY_J                  74
+#define VX_KEY_K                  75
+#define VX_KEY_L                  76
+#define VX_KEY_M                  77
+#define VX_KEY_N                  78
+#define VX_KEY_O                  79
+#define VX_KEY_P                  80
+#define VX_KEY_Q                  81
+#define VX_KEY_R                  82
+#define VX_KEY_S                  83
+#define VX_KEY_T                  84
+#define VX_KEY_U                  85
+#define VX_KEY_V                  86
+#define VX_KEY_W                  87
+#define VX_KEY_X                  88
+#define VX_KEY_Y                  89
+#define VX_KEY_Z                  90
+#define VX_KEY_LEFT_BRACKET       91  /* [ */
+#define VX_KEY_BACKSLASH          92  /* \ */
+#define VX_KEY_RIGHT_BRACKET      93  /* ] */
+#define VX_KEY_GRAVE_ACCENT       96  /* ` */
+#define VX_KEY_WORLD_1            161 /* non-US #1 */
+#define VX_KEY_WORLD_2            162 /* non-US #2 */
+
+/* Function keys */
+#define VX_KEY_ESCAPE             256
+#define VX_KEY_ENTER              257
+#define VX_KEY_TAB                258
+#define VX_KEY_BACKSPACE          259
+#define VX_KEY_INSERT             260
+#define VX_KEY_DELETE             261
+#define VX_KEY_RIGHT              262
+#define VX_KEY_LEFT               263
+#define VX_KEY_DOWN               264
+#define VX_KEY_UP                 265
+#define VX_KEY_PAGE_UP            266
+#define VX_KEY_PAGE_DOWN          267
+#define VX_KEY_HOME               268
+#define VX_KEY_END                269
+#define VX_KEY_CAPS_LOCK          280
+#define VX_KEY_SCROLL_LOCK        281
+#define VX_KEY_NUM_LOCK           282
+#define VX_KEY_PRINT_SCREEN       283
+#define VX_KEY_PAUSE              284
+#define VX_KEY_F1                 290
+#define VX_KEY_F2                 291
+#define VX_KEY_F3                 292
+#define VX_KEY_F4                 293
+#define VX_KEY_F5                 294
+#define VX_KEY_F6                 295
+#define VX_KEY_F7                 296
+#define VX_KEY_F8                 297
+#define VX_KEY_F9                 298
+#define VX_KEY_F10                299
+#define VX_KEY_F11                300
+#define VX_KEY_F12                301
+#define VX_KEY_F13                302
+#define VX_KEY_F14                303
+#define VX_KEY_F15                304
+#define VX_KEY_F16                305
+#define VX_KEY_F17                306
+#define VX_KEY_F18                307
+#define VX_KEY_F19                308
+#define VX_KEY_F20                309
+#define VX_KEY_F21                310
+#define VX_KEY_F22                311
+#define VX_KEY_F23                312
+#define VX_KEY_F24                313
+#define VX_KEY_F25                314
+#define VX_KEY_KP_0               320
+#define VX_KEY_KP_1               321
+#define VX_KEY_KP_2               322
+#define VX_KEY_KP_3               323
+#define VX_KEY_KP_4               324
+#define VX_KEY_KP_5               325
+#define VX_KEY_KP_6               326
+#define VX_KEY_KP_7               327
+#define VX_KEY_KP_8               328
+#define VX_KEY_KP_9               329
+#define VX_KEY_KP_DECIMAL         330
+#define VX_KEY_KP_DIVIDE          331
+#define VX_KEY_KP_MULTIPLY        332
+#define VX_KEY_KP_SUBTRACT        333
+#define VX_KEY_KP_ADD             334
+#define VX_KEY_KP_ENTER           335
+#define VX_KEY_KP_EQUAL           336
+#define VX_KEY_LEFT_SHIFT         340
+#define VX_KEY_LEFT_CONTROL       341
+#define VX_KEY_LEFT_ALT           342
+#define VX_KEY_LEFT_SUPER         343
+#define VX_KEY_RIGHT_SHIFT        344
+#define VX_KEY_RIGHT_CONTROL      345
+#define VX_KEY_RIGHT_ALT          346
+#define VX_KEY_RIGHT_SUPER        347
+#define VX_KEY_MENU               348
+
+#define VX_KEY_LAST               VX_KEY_MENU
+
+
+// Mouse IDs
+// From glfw3.h
+
+#define VX_MOUSE_BUTTON_1         0
+#define VX_MOUSE_BUTTON_2         1
+#define VX_MOUSE_BUTTON_3         2
+#define VX_MOUSE_BUTTON_4         3
+#define VX_MOUSE_BUTTON_5         4
+#define VX_MOUSE_BUTTON_6         5
+#define VX_MOUSE_BUTTON_7         6
+#define VX_MOUSE_BUTTON_8         7
+#define VX_MOUSE_BUTTON_LAST      VX_MOUSE_BUTTON_8
+#define VX_MOUSE_BUTTON_LEFT      VX_MOUSE_BUTTON_1
+#define VX_MOUSE_BUTTON_RIGHT     VX_MOUSE_BUTTON_2
+#define VX_MOUSE_BUTTON_MIDDLE    VX_MOUSE_BUTTON_3
