170f72f6cbebc4b309720a765d2a412564f5adc9
Sachin Cherian <sachinctl@protonmail.com>
2020-04-28 22:33:59 +0530

    remove gl version from glfw window hints to support multiple APIs

--- Vertex/src/Core/Window.h
+++ Vertex/src/Core/Window.h
@@ -30,20 +30,14 @@ namespace Vertex {
         bool v_sync;
         const char* title;
         unsigned int width, height;
-        unsigned int gl_major_version, gl_minor_version;
-        bool gl_use_compat;
 
         std::function<void(Event&)> event_callback;
 
         WindowProperties(const char* _title = "Vertex",
                          unsigned int _width = 1024,
                          unsigned int _height = 576,
-                         bool _v_sync = true,
-                         unsigned int _gl_major_version = 4,
-                         unsigned int _gl_minor_version = 6,
-                         bool _gl_use_compatibility_profile = false)
-            : title(_title), width(_width), height(_height), v_sync(_v_sync), gl_major_version(_gl_major_version),
-              gl_minor_version(_gl_minor_version), gl_use_compat(_gl_use_compatibility_profile)
+                         bool _v_sync = true)
+            : title(_title), width(_width), height(_height), v_sync(_v_sync)
         {
         }
     };
--- Vertex/src/Platforms/Linux/LinuxWindow.cpp
+++ Vertex/src/Platforms/Linux/LinuxWindow.cpp
@@ -172,7 +172,7 @@ namespace Vertex {
     LinuxWindow::LinuxWindow(WindowProperties properties)
         : m_Data(properties)
     {
-        Logger::GetCoreLogger()->info("Creating window {}", m_Data.title);
+        Logger::GetCoreLogger()->info("Creating window {0}", m_Data.title);
 
         if (!s_GLFW_Initialized)
         {
@@ -185,12 +185,6 @@ namespace Vertex {
         }
 
         glfwSetErrorCallback(GLFWErrorCallback);
-
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, m_Data.gl_major_version);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, m_Data.gl_minor_version);
-        
-        if(m_Data.gl_major_version > 3 || (m_Data.gl_major_version == 3 && m_Data.gl_minor_version >= 2))
-            glfwWindowHint(GLFW_OPENGL_PROFILE, m_Data.gl_use_compat ? GLFW_OPENGL_COMPAT_PROFILE : GLFW_OPENGL_CORE_PROFILE);
 
         m_Window = glfwCreateWindow((int)m_Data.width, (int)m_Data.height, m_Data.title, nullptr, nullptr);
 
