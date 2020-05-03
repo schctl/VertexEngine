f7f60a8d8a7e80d731597bd16aa72aa199ea14fd
Dinu Blanovschi <dinu.blanovschi001@gmail.com>
2020-05-03 13:44:48 +0300

    Windows build for vulkan in mingw compiles successfully

--- Vertex/src/Core/Core.h
+++ Vertex/src/Core/Core.h
@@ -6,15 +6,19 @@
     #define VX_CONFIGURATION_RELEASE
 #endif
 
-#if defined(_WIN32)
+#if defined(_MSC_VER)
     #if defined(VX_BUILD_SHARED)
         #define VX_PUBLIC_API __declspec(dllexport)
     #else
         #define VX_PUBLIC_API __declspec(dllimport)
     #endif // VX_BUILD_SHARED
-#else
-    #define VX_PUBLIC_API
-#endif // _WIN32
+#elif defined(__GNUC__)
+    #if defined(VX_BUILD_SHARED)
+        #define VX_PUBLIC_API __attribute__((visibility("default")))
+    #else
+        #define VX_PUBLIC_API extern
+    #endif // VX_BUILD_SHARED
+#endif // _MSC_VER/__GNUC__
 
 #define VX_BIND_FUNC_0(x) std::bind(&x, this)
 #define VX_BIND_FUNC_1(x) std::bind(&x, this, std::placeholders::_1)
