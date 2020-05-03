952393b4a75cb2e5fcac8633e89bde3b7a58c979
Sachin Cherian <sachinctl@protonmail.com>
2020-05-03 19:11:54 +0530

    change type to staticlib

--- Vertex/src/Core/Core.h
+++ Vertex/src/Core/Core.h
@@ -6,15 +6,7 @@
     #define VX_CONFIGURATION_RELEASE
 #endif
 
-#if defined(_WIN32)
-    #if defined(VX_BUILD_SHARED)
-        #define VX_PUBLIC_API __declspec(dllexport)
-    #else
-        #define VX_PUBLIC_API __declspec(dllimport)
-    #endif // VX_BUILD_SHARED
-#else
-    #define VX_PUBLIC_API
-#endif // _WIN32
+#define VX_PUBLIC_API
 
 #define VX_BIND_FUNC_0(x) std::bind(&x, this)
 #define VX_BIND_FUNC_1(x) std::bind(&x, this, std::placeholders::_1)
